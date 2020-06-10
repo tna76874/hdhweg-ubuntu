48645cc0fa65a4c8a2c5abc1d9c1da630719d649
lmh <lmh@local>
2020-06-10 11:11:38 +0200

    update install

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ REPODIR="/root/hdhweg-ubuntu"
 # delete the repository, if present. 
 rm -rf ${REPODIR}
 
-echo -e "update system sources and install ansible and git."
+echo -e "... update system sources, install ansible and git and clone the playbook repository ..."
 sudo apt update > /dev/null 2>&1
 sudo apt install software-properties-common -y > /dev/null 2>&1
 sudo apt-add-repository universe > /dev/null 2>&1
@@ -14,7 +14,7 @@ sudo apt-add-repository --yes --update ppa:ansible/ansible > /dev/null 2>&1
 sudo apt update > /dev/null 2>&1
 sudo apt install git ansible -y > /dev/null 2>&1
 
-echo -e "clone the playbook repository"
+#clone the playbook repository
 git clone https://github.com/tna76874/hdhweg-ubuntu.git ${REPODIR} > /dev/null 2>&1
 
 # run the playbook to set up the system
