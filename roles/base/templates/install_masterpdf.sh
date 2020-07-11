f5794ce0c693933e8cce1e387d3ce0cd2e3e3ad4
lmh <lmh@local>
2020-07-11 12:07:54 +0200

    fix masterpdf install script

--- roles/base/templates/install_masterpdf.sh
+++ roles/base/templates/install_masterpdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-VERSION=$(curl -s -L https://code-industry.net/checksum-information/ |  grep all.amd64.deb | awk '/master-pdf-editor-/,/amd64.deb/' | awk -v FS="(master-pdf-editor-|.amd64.deb)" '{print $2}')
-DEB=master-pdf-editor-"$VERSION".amd64.deb
+VERSION=$(curl -s -L https://code-industry.net/checksum-information/ |  grep qt5.x86_64.deb | awk '/master-pdf-editor-/,/-qt5.x86_64.deb/' | awk -v FS="(master-pdf-editor-|-qt5.x86_64.deb)" '{print $2}')
+DEB=master-pdf-editor-"$VERSION"-qt5.x86_64.deb
 
 AVAILABLE=$(echo ${VERSION} | awk -F- '{print $1}')
 INSTALLED=$(dpkg -l | grep master-pdf-editor | tr -s ' ' | tr -s ' ' | cut -d" " -f3)
