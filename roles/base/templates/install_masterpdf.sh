a82d52e2f11eb30a6376f256282ccf1c2f9bd54e
lmh <lmh@local>
2020-07-11 12:17:11 +0200

    switch masterpdf deb package

--- roles/base/templates/install_masterpdf.sh
+++ roles/base/templates/install_masterpdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-VERSION=$(curl -s -L https://code-industry.net/checksum-information/ |  grep qt5.x86_64.deb | awk '/master-pdf-editor-/,/-qt5.x86_64.deb/' | awk -v FS="(master-pdf-editor-|-qt5.x86_64.deb)" '{print $2}')
-DEB=master-pdf-editor-"$VERSION"-qt5.x86_64.deb
+VERSION=$(curl -s -L https://code-industry.net/checksum-information/ |  grep qt5_included.x86_64.deb | awk '/master-pdf-editor-/,/-qt5_included.x86_64.deb/' | awk -v FS="(master-pdf-editor-|-qt5_included.x86_64.deb)" '{print $2}')
+DEB=master-pdf-editor-"$VERSION"-qt5_included.x86_64.deb
 
 AVAILABLE=$(echo ${VERSION} | awk -F- '{print $1}')
 INSTALLED=$(dpkg -l | grep master-pdf-editor | tr -s ' ' | tr -s ' ' | cut -d" " -f3)
