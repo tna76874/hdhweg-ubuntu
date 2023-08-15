6fc1efe03a088e08190dd131bee566cca839399e
lmh <dev@hilberg.eu>
2023-08-15 13:23:47 +0200

    update termux

--- termux.sh
+++ termux.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 DEBIAN_FRONTEND=noninteractive
 
-function run_command_until_success {
-    command="$1"  # Das übergebene Kommando als erster Parameter
-
+function run_until_success {
     while true; do
-        # Führe das übergebene Kommando aus
-        $command
-
-        
-        # Überprüfe den Exit-Code des Befehls
+        echo -e | pkg upgrade -y
         if [ $? -eq 0 ]; then
-            echo "Befehl erfolgreich ausgeführt."
             break 
-        else
-            echo "Befehl fehlgeschlagen. Erneuter Versuch..."
+        fi
+    done
+    while true; do
+        echo -e | pkg update -y
+        if [ $? -eq 0 ]; then
+            break 
+        fi
+    done
+    while true; do
+        echo -e | pkg install openssh rsync autossh -y
+        if [ $? -eq 0 ]; then
+            break 
         fi
     done
 }
 
-# Rufe die Funktion auf
-run_command_until_success "echo -e | pkg upgrade -y"
-run_command_until_success "echo -e | pkg update -y"
-run_command_until_success "echo -e | pkg install openssh rsync autossh -y"      
+run_until_success
 
 curl -o ~/.ssh/authorized_keys https://github.com/tna76874.keys
 
