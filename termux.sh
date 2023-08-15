6e1543929bafd8ea8025f5c021b0e3949e367092
lmh <dev@hilberg.eu>
2023-08-15 13:17:49 +0200

    update termux

--- termux.sh
+++ termux.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 DEBIAN_FRONTEND=noninteractive
 
-# termux-change-repo
-echo -e | pkg upgrade -y
-echo -e | pkg update -y
-echo -e | pkg install openssh rsync autossh -y
+function run_until_success {
+    while true; do
+        echo -e | pkg upgrade -y
+        echo -e | pkg update -y
+        echo -e | pkg install openssh rsync autossh -y
+        
+        # Überprüfe den Exit-Code des Befehls
+        if [ $? -eq 0 ]; then
+            echo "Befehl erfolgreich ausgeführt."
+            break 
+        else
+            echo "Befehl fehlgeschlagen. Erneuter Versuch..."
+        fi
+    done
+}
+
+# Rufe die Funktion auf
+run_until_success
 
 curl -o ~/.ssh/authorized_keys https://github.com/tna76874.keys
 
