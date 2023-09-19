d9564f9e59333f74b58cc39d5238338713909533
lmh <dev@hilberg.eu>
2023-09-19 07:51:08 +0200

    update anaconda installer

--- roles/base/templates/install_anaconda.sh
+++ roles/base/templates/install_anaconda.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
-TMPDIR=${1:-"/tmp"}
+
+TMPDIR="/tmp"
+RUN_INSTALLER=true
+DOWNLOAD=true
+export BATCH=1
+
+while getopts ":dr" opt; do
+  case $opt in
+    d)
+        RUN_INSTALLER=false
+        DOWNLOAD=true
+        ;;
+    r)
+        RUN_INSTALLER=true
+        DOWNLOAD=false
+        ;;
+    \?)
+      echo "Ungültige Option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Die Option -$OPTARG erfordert ein Argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
 SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT")
 FIRSTDIR=${PWD}
 
-# getting latest teamspeak version
-VERSION=$(curl -s -L https://repo.anaconda.com/archive/ |  grep Linux-x86_64.sh | awk '/Anaconda3-/,/-Linux-x86_64.sh/' | awk -v FS="(Anaconda3-|-Linux-x86_64.sh)" '{print $2}' | tail -n 1)
+# getting latest anaconda version
+latest_script=$(curl -s -L https://repo.anaconda.com/archive/ | grep -oP 'Anaconda[0-9]*-\d{4}\.\d{2}-\d+-Linux-x86_64.sh' | sort -rV | head -n 1)
 
-# download the client
-cd ${TMPDIR}
-wget -O anaconda.sh --content-disposition https://repo.anaconda.com/archive/Anaconda3-${VERSION}-Linux-x86_64.sh
+if [ "$DOWNLOAD" = true ]; then
+  # download the client
+  cd ${TMPDIR}
+  wget -O anaconda.sh --content-disposition https://repo.anaconda.com/archive/${latest_script}
+  sed -i 's/BATCH=0/BATCH=1/' "${TMPDIR}/anaconda.sh"
+fi
 
-# run installer
-bash ${TMPDIR}/anaconda.sh
+if [ "$RUN_INSTALLER" = true ]; then
+  cd ${TMPDIR}
+  bash ${TMPDIR}/anaconda.sh
+fi
 
+# Zurück zum ursprünglichen Verzeichnis
 cd ${FIRSTDIR}
+
+exit 0
