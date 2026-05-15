29a4845630b39b07b0f829dc780e37ab4593815b
lmh <dev@hilberg.eu>
2026-05-15 12:22:01 +0200

    update inital setup script

--- setup.sh
+++ setup.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
-#getting some environment vars
-source /etc/lsb-release
 
-# setting the location of the ansible playbook repository
+# 1. Root check
+if [[ $EUID -ne 0 ]]; then
+   echo "Error: This script must be run as root (use sudo)."
+   exit 1
+fi
+
+set -e
+export DEBIAN_FRONTEND=noninteractive
+
 REPODIR="/root/hdhweg-ubuntu"
+rm -rf "${REPODIR}"
 
-# delete the repository, if present. 
-rm -rf ${REPODIR}
+echo -e "\n[1/4] Preparing system sources..."
+# Use -qq for apt to keep it quiet but allow critical errors to show
+apt-get update -qq
+apt-get install -y -qq software-properties-common > /dev/null
 
-echo -e "... update system sources, install ansible and git and clone the playbook repository ... [this could take a few minutes now, depending on your internet connection]"
-sudo apt update > /dev/null 2>&1
-sudo apt install software-properties-common -y > /dev/null 2>&1
-sudo apt-add-repository universe > /dev/null 2>&1
-sudo apt-add-repository multiverse > /dev/null 2>&1
-sudo apt-add-repository --yes --update ppa:ansible/ansible > /dev/null 2>&1
-sudo apt update > /dev/null 2>&1
-sudo apt install git ansible -y > /dev/null 2>&1
+echo "[2/4] Adding software repositories..."
+apt-add-repository -y universe > /dev/null
+apt-add-repository -y multiverse > /dev/null
+apt-add-repository --yes --update ppa:ansible/ansible > /dev/null
 
-#clone the playbook repository
-git clone https://github.com/tna76874/ubuntu-desktop.git ${REPODIR} > /dev/null 2>&1
+echo "[3/4] Installing Ansible and Git..."
+apt-get install -y -qq git ansible > /dev/null
 
-# run the playbook to set up the system
-cd ${REPODIR}
-sudo ansible-playbook main.yml -t ${1-setup}
+echo "[4/4] Downloading and running configuration..."
+git clone https://github.com/tna76874/ubuntu-desktop.git "${REPODIR}" --quiet
+cd "${REPODIR}"
+
+# Running the playbook
+# We don't silence this completely so the user sees the final progress
+ansible-playbook main.yml -t "${1-setup}"
